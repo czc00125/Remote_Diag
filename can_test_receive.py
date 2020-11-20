201c59cd45f2c9ad2faee0e1bd900499b061ea70
Zhou Fall <18860901456@163.com>
2020-11-20 17:28:06 +0800

    1.Remove useless files 2. Add linux Tcp tool Update by Zhou Fall
    Signed-off-by: Zhou Fall <18860901456@163.com>

--- can_test_receive.py
+++
@@ -1,9 +0,0 @@
-import can
-
-can_interface = 'can0'
-bus = can.interface.Bus(can_interface, bustype='socketcan_native')
-while True:
-    message = bus.recv(10.0)  # Timeout in seconds.
-    if message is None:
-        print('Timeout occurred, no message.')
-    print(message)
